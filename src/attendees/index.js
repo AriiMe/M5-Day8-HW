4e9ebefdc89536811b7c8a5a5a5db47aba59ceee
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-12-16 17:38:31 +0100

    yeet

--- src/attendees/index.js
+++ src/attendees/index.js
@@ -3,7 +3,10 @@ const attendeesRouter = express.Router();
 const {getAttendees,writeAttendees} = require("../fsUtilities")
 const uniqid = require("uniqid")
 const sgMail = require("@sendgrid/mail")
-
+const { join } = require("path");
+const { createReadStream } = require("fs-extra");
+const { pipeline } = require("stream");
+const { Transform } = require("json2csv");
 
 attendeesRouter.post("/", async(req,res,next) => {
     try {
@@ -42,6 +45,30 @@ attendeesRouter.post("/", async(req,res,next) => {
 attendeesRouter.get("/test", (req,res,next) => {
     res.status(200).send("test attendees working")
 })
+
+attendeesRouter.get("/export/csv", (req, res, next) => {
+    try {
+      const pathToJson = join(__dirname, "attendees.json"); //CREATING PATH TO JSON FILE
+      const jsonReadableStream = createReadStream(pathToJson); //CREATES SOURCE FOR PIPELINE
+      //SOURCE IS THE FILE THAT WE'RE SENDING
+  
+      const json2csv = new Transform({
+        fields: ["FirstName", "Surname", "ToA", "Email", "ID"],
+      }); //TURNS JSON FILE INTO A CSV FILE WITH THE FIELDS AS COLUMNS
+  
+      res.setHeader("Content-Disposition", "attachment; filename=export.csv"); //SETS ENDPOINT TO DOWNLOAD THE FILES IN THE RESPONSE
+      pipeline(jsonReadableStream, json2csv, res, (err) => {
+        if (err) {
+          console.log(err);
+          next(err);
+        } else {
+          console.log("THIS ONE WORKS");
+        }
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
 
 
 module.exports = attendeesRouter;
