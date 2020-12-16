2230adff0312074e3356d99884026636d222d0f7
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-12-16 15:29:04 +0100

    sendgrid added

--- src/attendees/index.js
+++ src/attendees/index.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const attendeesRouter = express.Router();
+const {readDB,writeDB} = require("../fsUtilities")
+const uniqid = require("uniqid")
+const sgMail = require("@sendgrid/mail")
+
 
 attendeesRouter.post("/", async(req,res,next) => {
-    
+    try {
+        const attDB = await readDB()
+        attDB.push({
+            ...req.body,
+            ID: uniqid(),
+        })
+        await writeDB(attDB)
+        
+        try {
+            sgMail.setApiKey(process.eventNames.SENDGRID_API)
+
+        } catch (error) {
+            console.log(error)
+            next(error)
+        }
+        
+
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
 })
 
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const listEndpoints = require("express-list-endpoints");
+const {
+  notFoundHandler,
+  badRequestHandler,
+  genericErrorHandler,
+} = require("./errorHandlers");
 
 const attendeesRoutes = require("./attendees");
 
@@ -11,17 +16,17 @@ const port = process.env.PORT || 3001;
 server.use(express.json());
 server.use(cors());
 
-
-
 server.use("/attendees", attendeesRoutes);
 
-
+server.use(badRequestHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
 
 console.log(listEndpoints(server));
 
 server.listen(port, () => {
   if (process.env.NODE_ENV === "production") {
-    console.log("Running on cloud port", port);
+    console.log("Running on cloud on port", port);
   } else {
     console.log("Running locally on port", port);
   }
