e39960a01ed7354e4a41eede3e5b15c7a21b556f
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-12-16 14:48:59 +0100

    ye

---
+++ src/attendees/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const attendeesRouter = express.Router();
+
+attendeesRouter.post("/", async(req,res,next) => {
+    
+})
+
+
+module.exports = attendeesRouter;
--- src/server.js
+++ src/server.js
@@ -1,29 +1,28 @@
-const express = require("express")
-const cors = require("cors")
-const listEndpoints = require("express-list-endpoints")
+const express = require("express");
+const cors = require("cors");
+const listEndpoints = require("express-list-endpoints");
 
-const booksRoutes = require("./services/books")
+const attendeesRoutes = require("./attendees");
+
+const server = express();
+
+const port = process.env.PORT || 3001;
+
+server.use(express.json());
+server.use(cors());
 
 
-const server = express()
 
-const port = process.env.PORT || 3001 // the fallback is for local development, heroku will use his own port, something like 12312, because imagine how many processes are running on the same machine there
+server.use("/attendees", attendeesRoutes);
 
-server.use(express.json())
-server.use(cors()) // CROSS ORIGIN RESOURCE SHARING
 
-//ROUTES
 
-server.use("/books", booksRoutes)
-
-// ERROR HANDLERS
-
-console.log(listEndpoints(server))
+console.log(listEndpoints(server));
 
 server.listen(port, () => {
-    if (process.env.NODE_ENV === "production") {
-        console.log("Running on cloud on port", port)
-    } else {
-        console.log("Running locally on port", port)
-    }
-})
+  if (process.env.NODE_ENV === "production") {
+    console.log("Running on cloud port", port);
+  } else {
+    console.log("Running locally on port", port);
+  }
+});
