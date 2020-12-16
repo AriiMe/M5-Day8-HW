bf7a638dd661c1c0f3edaa41565cf50787a6985f
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-12-16 15:40:01 +0100

    write

--- src/attendees/index.js
+++ src/attendees/index.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const attendeesRouter = express.Router();
-const {readDB,writeDB} = require("../fsUtilities")
+const {getAttendees,writeAttendees} = require("../fsUtilities")
 const uniqid = require("uniqid")
 const sgMail = require("@sendgrid/mail")
 
 
 attendeesRouter.post("/", async(req,res,next) => {
     try {
-        const attDB = await readDB()
+        const attDB = await getAttendees()
         attDB.push({
             ...req.body,
             ID: uniqid(),
         })
-        await writeDB(attDB)
-        
+        await writeAttendees(attDB)
+
         try {
             sgMail.setApiKey(process.eventNames.SENDGRID_API)
+
+            const msg = {
+                to: req.body.Email,
+                from: "anothergamer69@gmail.com",
+                subject: "You bastard",
+                text: "sucky sucky bimbaki!",
+            }
+
+            await sgMail.send(msg)
+            res.send("EMAIL YEETED")
 
         } catch (error) {
             console.log(error)
